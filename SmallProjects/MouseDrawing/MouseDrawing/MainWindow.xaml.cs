649c75fe82ab5c44627e33c8c07e1051170c6a3c
Jardee <aeduel@gmail.com>
2018-12-29 00:06:28 +0100

    MouseDrawing

--- SmallProjects/MouseDrawing/MouseDrawing/MainWindow.xaml.cs
+++ SmallProjects/MouseDrawing/MouseDrawing/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -9,11 +10,15 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Clipboard = System.Windows.Clipboard;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace MouseDrawing
 {
@@ -26,11 +31,87 @@ namespace MouseDrawing
         ImageSource grap;
         public MainWindow()
         {
+            //MessageBox.Show(Screen.PrimaryScreen.Bounds.Width.ToString() + this.Width.ToString());
+            
             InitializeComponent();
             delaytxt.Text = "20";
             pixskiptxt.Text = "2";
+            this.Left = Screen.PrimaryScreen.Bounds.Width - this.Width;
+            this.Top = 0;
+            this.Height = Screen.PrimaryScreen.Bounds.Height - 40;
+            getimages(@"C:\imgs");
         }
 
+        private void getimages(string folderpath)
+        {
+            if (folderpath == "") return;
+            //MessageBox.Show("1");
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(folderpath);
+
+                if (dir.Exists)
+                {
+                    foreach (var item in dir.GetFiles())
+                    {
+                        if (".jpg|.jpeg|.gif|.png".Contains(item.Extension.ToLower()))
+                        {
+                            addimage(item.FullName);
+                            //MessageBox.Show("No witam");
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private void addimage(string pth)
+        {
+            Image img = new Image();
+
+            BitmapImage src = new BitmapImage();
+
+            src.BeginInit();
+
+            src.UriSource = new Uri(pth, UriKind.Absolute);
+
+            src.EndInit();
+
+            img.Source = src;
+
+            img.Stretch = Stretch.Uniform;
+            img.Height = 25;
+
+            img.MouseDown += Img_MouseDown;
+            childs.Children.Add(img);
+
+        }
+
+        private void Img_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Image ctrl = (Image)sender;
+            grap = ctrl.Source;
+            sd = (BitmapSource)grap;
+
+            int delay = 20;
+            if (int.TryParse(delaytxt.Text, out int ddx)) delay = ddx;
+            int pixelskip = 2;
+            if (int.TryParse(pixskiptxt.Text, out int ps)) pixelskip = ps;
+
+            OpacityWindow preview = new OpacityWindow(sd, delay, pixelskip);
+            preview.Width = sd.PixelWidth;
+            preview.Height = sd.PixelHeight;
+
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource = ctrl.Source;
+            preview.Background = myBrush;
+
+            preview.Show();
+        }
 
         private void Image_KeyDown(object sender, KeyEventArgs e)
         {
@@ -39,166 +120,17 @@ namespace MouseDrawing
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.V && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
-            {
-                BitmapSource tmp = Clipboard.GetImage();
-                this.imagebox.SetCurrentValue(Image.SourceProperty, tmp);
-                lbll.Content = tmp.PixelHeight + " x " + tmp.PixelWidth;
-                //DoMouseClick();
-            }
-
-            if (e.Key == Key.D)
-            {
-                delaytxt.IsEnabled = false;
-                pixskiptxt.IsEnabled = false;
-                if (imagebox.Source != null)
-                {
-                    //SetCursorPos(0, 0);
-                    int delay = 20;
-                    if (int.TryParse(delaytxt.Text, out int ddx)) delay = ddx;
-                    int pixelskip = 2;
-                    if (int.TryParse(pixskiptxt.Text, out int ps)) pixelskip = ps;
-                    grap = imagebox.Source;
-                    sd = (BitmapSource)grap;
-                    MousePoint dd;
-                    GetCursorPos(out dd);
-                    bool first = true;
-                    bool previous = false;
-                    //long ix = 0;
-                    //long max = sd.PixelHeight * sd.PixelWidth;
-                    
-                    for (int x = 0;x < sd.PixelWidth; x += pixelskip)
-                    {
-                        for (int y = 0;y < sd.PixelHeight; y += pixelskip)
-                        {
-                            first = GetPixelColor(sd, x, y);
-                            
-                            if (first && !previous)
-                            {
-                                
-                                SetCursorPos(dd.X + x, dd.Y + y);
-                                Grab();
-                                    
-                                
-                                //ix++;
-                                //lbll.Content = ix.ToString() + " / " + max.ToString();
-                                //this.UpdateLayout();
-                                
-                            }
-                            else if (!first && previous)
-                            {
-                                Release();
-                                
-                            }
-
-                            if (first && previous)
-                            {
-                                SetCursorPos(dd.X + x, dd.Y + y);
-                                Thread.Sleep(delay);
-                            }
-                            previous = first;
-                        }
-                        
-                    }
-                    
-                    //MessageBox.Show(GetPixelColor(sd, 10, 10).ToString());
-                }
-                else
-                {
-                    MessageBox.Show("wklej grafike!");
-                }
-                }
-        }
-
-        public static bool GetPixelColor(BitmapSource bitmap, int x, int y)
-        {
-            Color color;
-            var bytesPerPixel = (bitmap.Format.BitsPerPixel + 7) / 8;
-            var bytes = new byte[bytesPerPixel];
-            var rect = new Int32Rect(x, y, 1, 1);
-
-            bitmap.CopyPixels(rect, bytes, bytesPerPixel, 0);
-
-
-
-                if (bytes[2] > 200 && bytes[1] > 200 && bytes[0] > 200)
-                {
-                    //MessageBox.Show("> 240");
-                    return false;
-
-                }
-                else
-                {
-                    //MessageBox.Show("<240");
-                    return true;
-                }
-
             
-            //MessageBox.Show("Nic");
-            return false;
         }
+
+
 
         private void BtnDraw_Click(object sender, RoutedEventArgs e)
         {
             
         }
 
-        [System.Runtime.InteropServices.DllImport("user32")]
-        public static extern int SetCursorPos(int x, int y);
 
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
-        //Mouse actions
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
-        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
-        private const int MOUSEEVENTF_RIGHTUP = 0x10;
-
-        public static void Grab()
-        {
-            MousePoint x;
-            GetCursorPos(out x);
-            uint X = (uint)x.X;
-            uint Y = (uint)x.Y;
-            mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
-        }
-
-        public static void Release()
-        {
-            MousePoint x;
-            GetCursorPos(out x);
-            uint X = (uint)x.X;
-            uint Y = (uint)x.Y;
-            mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
-        }
-
-
-        public void DoMouseClick()
-        {
-            //Call the imported function with the cursor's current position
-            MousePoint x;
-            GetCursorPos(out x);
-            uint X = (uint)x.X;
-            uint Y = (uint)x.Y;
-            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MousePoint
-        {
-            public int X;
-            public int Y;
-
-            public MousePoint(int x, int y)
-            {
-                X = x;
-                Y = y;
-            }
-        }
 
         private void Imagebox_SourceUpdated(object sender, DataTransferEventArgs e)
         {
@@ -209,6 +141,12 @@ namespace MouseDrawing
         {
             delaytxt.IsEnabled = true;
             pixskiptxt.IsEnabled = true;
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            //Window window = (Window)sender;
+           // window.Topmost = true;
         }
     }
 }
---
+++ SmallProjects/MouseDrawing/MouseDrawing/OpacityWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace MouseDrawing
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OpacityWindow.xaml
+    /// </summary>
+    public partial class OpacityWindow : Window
+    {
+        BitmapSource glbl;
+        int pixelskip;
+        int delay;
+
+        public OpacityWindow(BitmapSource bd, int dl, int skp)
+        {
+            InitializeComponent();
+            glbl = bd;
+            pixelskip = skp;
+            delay = dl;
+            Timer tmt = new Timer();
+            tmt.Interval = 50;
+            tmt.Tick += Tmt_Tick;
+            tmt.Start();
+        }
+
+        private void Tmt_Tick(object sender, EventArgs e)
+        {
+            MousePoint dd;
+            GetCursorPos(out dd);
+            this.Top = dd.Y;
+            this.Left = dd.X;
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public static void Grab()
+        {
+            MousePoint x;
+            GetCursorPos(out x);
+            uint X = (uint)x.X;
+            uint Y = (uint)x.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
+        }
+
+        public static void Release()
+        {
+            MousePoint x;
+            GetCursorPos(out x);
+            uint X = (uint)x.X;
+            uint Y = (uint)x.Y;
+            mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+
+        public void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            MousePoint x;
+            GetCursorPos(out x);
+            uint X = (uint)x.X;
+            uint Y = (uint)x.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MousePoint
+        {
+            public int X;
+            public int Y;
+
+            public MousePoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        public static bool GetPixelColor(BitmapSource bitmap, int x, int y)
+        {
+            Color color;
+            var bytesPerPixel = (bitmap.Format.BitsPerPixel + 7) / 8;
+            var bytes = new byte[bytesPerPixel];
+            var rect = new Int32Rect(x, y, 1, 1);
+
+            bitmap.CopyPixels(rect, bytes, bytesPerPixel, 0);
+
+            if (bytes[2] > 200 && bytes[1] > 200 && bytes[0] > 200) return false;
+            else return true;
+
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            this.Hide();
+
+            if (e.Key == Key.D)
+            {
+
+                BitmapSource sd = glbl;
+                MousePoint dd;
+                GetCursorPos(out dd);
+                bool first = true;
+                bool previous = false;
+
+                for (int x = 0; x < sd.PixelWidth; x += pixelskip)
+                {
+                    for (int y = 0; y < sd.PixelHeight; y += pixelskip)
+                    {
+                        first = GetPixelColor(sd, x, y);
+
+                        if (first && !previous)
+                        { 
+                            SetCursorPos(dd.X + x, dd.Y + y);
+                            Grab();
+
+                        }
+                        else if (!first && previous) Release();
+
+                        if (first && previous)
+                        {
+                            SetCursorPos(dd.X + x, dd.Y + y);
+                            System.Threading.Thread.Sleep(delay);
+                        }
+
+                        previous = first;
+                    }
+
+                }
+
+                this.Close();
+            }
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+        }
+    }
+}
